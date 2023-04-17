d64b28ab13088394b3072edab524ac9a31f1de6b
mrafalcon <asokolov.photo@gmail.com>
2023-04-17 21:23:09 +0300

    html_js

--- html_js/js/script.js
+++ html_js/js/script.js
@@ -1,13 +1,22 @@
 let x = 0; 
 function myFunction() {
 	console.log("Hello, world! ", x);
-	if (x>=5) {
-		console.log("X > 10! ", x%10, " ", x);
+	if (x >= 5) {
+		console.log("test_count_var_1 >=5! ", x % 10, " ", x);
 		x = -5;
-	} else if (x%2 == 1) {
+	} else if (x<=0) {
+		document.getElementById("heading1").style.color = 'black';
+	} else if (x % 2 == 1) {
     	document.getElementById("demo").innerHTML = "Привет, javascript!";
+		document.getElementById("heading1").style.color = 'blue';
 	} else {
 		document.getElementById("demo").innerHTML = "Пока, javascript!";
+		document.getElementById("heading1").style.color = 'red';
 	};
-	x = x+1;
+	x = x + 1;
+}
+
+
+function insertFunction () {
+	
 }

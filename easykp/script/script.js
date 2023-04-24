83ba82dbb2cb8915419f9038a8837712ea6d9efa
Andrey <asokolov.photo@gmail.com>
2023-04-24 16:52:42 +0300

    попытка сделать фильтр таблицы

--- easykp/script/script.js
+++ easykp/script/script.js
@@ -58,24 +58,23 @@ setInterval(function getCountRows() {
 
 
 
-function searchContent() {
-    // Объявить переменные
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("documentsfordiadoc");
-    tr = table.getElementsByTagName("tr");
-  
-    // Перебирайте все строки таблицы и скрывайте тех, кто не соответствует поисковому запросу
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[2];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
+function searchContent(input) {
+  // Объявить переменные
+  var filter, table, tr, td, i, txtValue;
+  filter = input.toUpperCase();
+  table = document.getElementById("documentsfordiadoc");
+  tr = table.getElementsByTagName("tr");
+
+  // Перебирайте все строки таблицы и скрывайте тех, кто не соответствует поисковому запросу
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
+}

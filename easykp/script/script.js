afa9a83f80ed2806653766f2e8841994a649bd77
Andrey <asokolov.photo@gmail.com>
2023-04-24 15:41:17 +0300

    добавлена таблица (поля с данными статичны)

--- easykp/script/script.js
+++ easykp/script/script.js
@@ -51,3 +51,31 @@ function openStatus2() {
 }
 
 
+
+setInterval(function getCountRows() {
+    document.getElementById("countrows").innerHTML = "Общее число строк в отчете - "+(document.getElementById("documentsfordiadoc").rows.length -1);
+}, 1);
+
+
+
+function searchContent() {
+    // Объявить переменные
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("documentsfordiadoc");
+    tr = table.getElementsByTagName("tr");
+  
+    // Перебирайте все строки таблицы и скрывайте тех, кто не соответствует поисковому запросу
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }

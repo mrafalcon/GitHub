3e87502ec4c44db56589f873294e834db115aff1
mrafalcon <asokolov.photo@gmail.com>
2023-07-04 20:55:36 +0300

    0

--- python/test_text.py
+++ python/test_text.py
@@ -1,19 +1,26 @@
 import os
 import pandas as pd
 
+#from progress.bar import Bar
+
 cwd = os.getcwd()
 cwd
-os.chdir(r"C:\Users\gz1\Downloads\GitHub\GitHub\python")
+os.chdir("/Users/andreisokolov/Documents/GitHub/GitHub/python/")
 os.listdir('.')
 
 content_array = []
 
-file = 'test_text.txt'
-with open(file, "r") as file:
+file = "test_text.txt"
+
+
+with open(file, "r", encoding= "cp1251") as file:
     content = file.read()
 content = content.split('\n')
+#bar = Bar('Processing', max=len(content))
 for i in range(len(content)):
     content_array.append(content[i].split('.'))
+    #bar.next()
+#bar.finish()
 
 
 print(content_array)

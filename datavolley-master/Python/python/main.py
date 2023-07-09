788f403a3558cd08f7ddadd45c411945084f4c82
mrafalcon <asokolov.photo@gmail.com>
2023-07-09 20:31:57 +0300

    datavolley - python load and split

--- datavolley-master/Python/python/main.py
+++ datavolley-master/Python/python/main.py
@@ -2,10 +2,10 @@ from tabulate import tabulate
 import load
 
 load.init()
-load.importFile("datavolley-master/Python/data/","testing_truncated.dvw")
+load.importFile("datavolley-master/Python/data/","example_data.dvw")
 
 
-head = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27']
+#head = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27']
  
 #print(tabulate(load.set1[4:10], headers=head, tablefmt="grid"))
-print(tabulate(load.set1[4:-1]))
+print(tabulate(load.set2[4:-1]))
